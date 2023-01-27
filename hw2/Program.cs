aa41407188ae26cab539c7586e5dd95b90cd706f
Spodareva Yana <arnaut_yana@mail.ru>
2023-01-27 20:15:50 +0300

    комит1

---
+++ hw2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int result1 = num % 10;
+int result2 = (num / 10) % 10;
+// int result3 =
+
+Console.WriteLine(result2);
